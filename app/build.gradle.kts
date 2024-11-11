94c8c1d49d1cf4c7568e59a93cd9e47045784bb4
Niko Diamadis <niko@cyb3rko.de>
2024-11-10 21:15:24 -0500

    chore: publish version 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.cyb3rko.quicktrust"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         resourceConfigurations.add("en")
         signingConfig = signingConfigs.getByName("debug")
     }
